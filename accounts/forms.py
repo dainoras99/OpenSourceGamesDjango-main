ef0770b978c7139bb0cc6e3c94b5d40e9aa67529
KatinasPukis <65959494+KatinasPukis@users.noreply.github.com>
2021-11-10 00:26:53 +0200

    update

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,12 +9,27 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model=User
         fields=['username','email','password1','password2']
-        
+        labels = {
+            'username': 'Vartotojo vardas',
+            'email': 'El.Paštas',
+            'password1': 'Slaptažodis',
+            'password2': 'Pakartokite slaptažodį',
+        }
 
 class CreateNewGame(ModelForm):
     class Meta:
         model=UserGame
         fields=['gameName','gameDescription','gameCode', 'image','gameOwnerUsername']
+        labels = {
+            'gameName': 'Žaidimo pavadinimas',
+            'gameDescription': 'Žaidimo aprašymas',
+            'gameCode': 'Žaidimo kodas',
+            'image': 'Žaidimo nuotraukos įkelimas',
+        }
+        widgets={
+            'gameDescription' : forms.Textarea(attrs={'class':'form-control', 'rows':'3'}),
+            'gameCode' : forms.Textarea(attrs={'class':'form-control','rows':'3'}),
+        }
         
         
 
@@ -22,6 +37,15 @@ class CreateNewNews(ModelForm):
     class Meta:
         model=NewsClass
         fields=['headline','author','text', 'image']
+        labels = {
+            'headline': 'Antraštė',
+            'author': 'Autorius',
+            'text': 'Naujiena',
+            'image': 'Naujienos nuotrauka',
+        }
+        widgets={
+            'text' : forms.Textarea(attrs={'class':'form-control', 'rows':'3'}),
+        }
 
 class CreateNewComment(ModelForm):
     class Meta:
